340fd7c8b5e081f54ca01a3e353e703106edbca9
agustina <agustina.amed@gmail.com>
2020-04-21 17:32:54 -0300

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer); // JSON convierte un objeto a un formato JSON valido
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+    });
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion) // para colocarle indices a los registros del archivo
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    // Utilizamos una funcion FILTER que va a filtrar lo que busquemos y crear un nuevo arreglo total para el archivo sin ese campo
+    let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion
+    });
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
